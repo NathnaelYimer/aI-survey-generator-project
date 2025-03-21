8501ddc5c0334134470f22036b517c2a982c61e2
Nathnael Yimer <natnaelyimer2040@gmail.com>
2025-03-21 11:58:34 +0300

    removing the eslitrc rules

---
+++ .eslintrc.js
@@ -0,0 +1,7 @@
+module.exports = {
+    rules: {
+      'react/no-unescaped-entities': 'off', // Disable the unescaped entities rule
+      '@typescript-eslint/no-unused-vars': 'off', // Disable the unused variables rule
+    },
+  };
+  
